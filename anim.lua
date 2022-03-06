a378e1729dcfb45f1454c47ab8bd7868f73f8e1e
Coldlolololir <78960389+Coldlolololir@users.noreply.github.com>
2022-03-06 14:39:27 +0100

    Create anim.lua

---
+++ anim.lua
@@ -0,0 +1,11 @@
+local Players = game:GetService('Players')
+local Animate = Players.LocalPlayer.Character.Animate
+Animate.idle.Animation1.AnimationId = 'rbxassetid://5319828216'
+Animate.idle.Animation2.AnimationId = 'rbxassetid://5319831086'
+Animate.run.RunAnim.AnimationId = 'rbxassetid://5319847204'
+Animate.walk.WalkAnim.AnimationId = 'rbxassetid://5319847204'
+Animate.jump.JumpAnim.AnimationId = 'rbxassetid://5319841935'
+Animate.climb.ClimbAnim.AnimationId = 'rbxassetid://5319816685'
+Animate.fall.FallAnim.AnimationId = 'rbxassetid://5319839762'
+Animate.swim.Swim.AnimationId = 'rbxassetid://5319850266'
+Animate.swimidle.SwimIdle.AnimationId = 'rbxassetid://5319852613'
